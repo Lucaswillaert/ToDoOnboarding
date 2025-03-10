793712c44ef829b454b44acdf1d8560eb0792570
Lucaswillaert <lucas.willaert@student.ehb.be>
2025-03-10 15:30:46 +0100

    added ApiProperties to the response

--- src/modules/todo/use-cases/create-todo/create-todo.response.ts
+++ src/modules/todo/use-cases/create-todo/create-todo.response.ts
@@ -1,12 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Todo } from '../../entities/todo.entity.js'
 
 export class CreateTodoResponse {
   uuid: string
+  @ApiProperty({ type: String, format: 'date-time' })
   createdAt: string
+
+  @ApiProperty({ type: String, format: 'date-time' })
   updatedAt: string
+
+  @ApiProperty()
   title: string
+
+  @ApiProperty({ type: String, nullable: true })
   description: string | null
+
+  @ApiProperty({ type: String, nullable: true, format: 'date-time' })
   deadline: string | null
+
+  @ApiProperty()
   isCompleted: boolean | null
 
   constructor (todo: Todo) {
