e2c273a3bc1b2a5309477653915c985cf7f5f052
Lucaswillaert <lucas.willaert@student.ehb.be>
2025-03-12 16:39:02 +0100

    deleted AuthModule from createTodo/getAllTodos Modules ==> Added to the global TodoModule

--- src/modules/todo/todo.module.ts
+++ src/modules/todo/todo.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common'
+import { AuthModule } from '../auth/auth.module.js'
 import { CreateTodoModule } from './use-cases/create-todo/create-todo.module.js'
+import { GetAllTodosModule } from './use-cases/get-all-todos/get-all-todos.module.js'
 
 @Module({
   imports: [
-    CreateTodoModule
+    CreateTodoModule,
+    GetAllTodosModule,
+    AuthModule
   ]
 })
 export class TodoModule {}
