46cddbc9176ad2cb10a73df32a05f5961d1d9992
Lucaswillaert <lucas.willaert@student.ehb.be>
2025-03-18 14:25:37 +0100

    added a command and response for the update

---
+++ src/modules/todo/use-cases/update-todo/update-todo.command.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsDateString, IsNotEmpty, IsString } from 'class-validator'
+import { IsNullable } from '@wisemen/validators'
+
+export class UpdateTodoCommand {
+  @ApiProperty({ type: String })
+  @IsNotEmpty()
+  title: string
+
+  @ApiProperty({ type: String })
+  @IsNullable()
+  @IsString()
+  description: string
+
+  @ApiProperty({ type: String, nullable: true, format: 'date-time' })
+  @IsDateString({ strict: true })
+  deadline: string | null
+}
