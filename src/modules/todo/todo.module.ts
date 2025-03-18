69002ba708bacadfcb94cd015b0eaf67d353290f
Lucaswillaert <lucas.willaert@student.ehb.be>
2025-03-18 11:57:06 +0100

    Added permission for the get-todo use case
    added the get-todo module to the general todo module

--- src/modules/todo/todo.module.ts
+++ src/modules/todo/todo.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { CreateTodoModule } from './use-cases/create-todo/create-todo.module.js'
+import { GetTodoModule } from './use-cases/get-todo/get-todo.module.js'
 
 @Module({
   imports: [
-    CreateTodoModule
+    CreateTodoModule,
+    GetTodoModule
   ]
 })
 export class TodoModule {}
