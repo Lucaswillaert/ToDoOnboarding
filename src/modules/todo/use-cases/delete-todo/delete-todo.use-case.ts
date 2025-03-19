9f8efdb897d25dbddeae1bc3a8d3edbf1e25b516
Lucaswillaert <lucas.willaert@student.ehb.be>
2025-03-19 10:56:47 +0100

    deleted unused import

--- src/modules/todo/use-cases/delete-todo/delete-todo.use-case.ts
+++ src/modules/todo/use-cases/delete-todo/delete-todo.use-case.ts
@@ -1,14 +1,13 @@
 import { InjectRepository } from '@wisemen/nestjs-typeorm'
 import { Injectable } from '@nestjs/common'
-import { DataSource, Repository } from 'typeorm'
+import { Repository } from 'typeorm'
 import { Todo } from '../../entities/todo.entity.js'
 
 @Injectable()
 export class DeleteTodoUseCase {
   constructor (
     @InjectRepository(Todo)
-    private readonly todoRepository: Repository<Todo>,
-    private readonly dataSource: DataSource
+    private readonly todoRepository: Repository<Todo>
   ) {}
 
   async execute (uuid: string): Promise<void> {
